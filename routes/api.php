153c061775c034db4e267ee944232ebe7f8b0901
dsnakeall-crypto <dsnakeall@gmail.com>
2026-09-01 15:22:36 -0300

    fix: ordenar imports da API para o Pint

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\ClientController;
 use App\Http\Controllers\CatalogController;
+use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ServiceOrderController;
 use Illuminate\Support\Facades\Route;
 
