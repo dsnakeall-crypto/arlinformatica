7eddfbe7d15c81ad7b9b496c7ae2bf69ed74976f
dsnakeall-crypto <dsnakeall@gmail.com>
2026-09-01 21:36:17 -0300

    fix: ajustar formatação Pint da etapa 8

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Http\Controllers\BudgetController;
 use App\Http\Controllers\BackupController;
+use App\Http\Controllers\BudgetController;
 use App\Http\Controllers\CatalogController;
 use App\Http\Controllers\ClientController;
-use App\Http\Controllers\DocumentController;
 use App\Http\Controllers\DiagnosticController;
+use App\Http\Controllers\DocumentController;
 use App\Http\Controllers\FinalizationController;
 use App\Http\Controllers\FinanceController;
 use App\Http\Controllers\NotificationController;
