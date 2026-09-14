136c215a59e520814351b9bfe81c437d868381fc
dsnakeall-crypto <dsnakeall@gmail.com>
2026-09-14 00:34:48 -0300

    style: atualizar timbrado dos PDFs da OS

--- app/Services/OfficialLetterhead.php
+++ app/Services/OfficialLetterhead.php
@@ -10,13 +10,13 @@ class OfficialLetterhead
 
     public const HEIGHT = 1491;
 
-    public const SHA256 = '5e640a129a7b33d954e9f3b44a872b6f003ed8266f30039a578c11fe599e0087';
+    public const SHA256 = '8e900b1c8d8427c0bf1d3c9cbe80807131b7f704d22a2ba2babc437ecd2a5f21';
 
     public static function path(): string
     {
         // This service is also used by a plain PHPUnit test, before Laravel's
         // application container (and therefore resource_path()) is available.
-        $path = dirname(__DIR__, 2).'/resources/images/documents/papel-timbrado.png';
+        $path = dirname(__DIR__, 2).'/resources/images/documents/papel-timbrado-novo.png';
         $size = @getimagesize($path);
 
         if ($size === false || $size[0] !== self::WIDTH || $size[1] !== self::HEIGHT || hash_file('sha256', $path) !== self::SHA256) {
--- tests/Unit/OfficialLetterheadTest.php
+++ tests/Unit/OfficialLetterheadTest.php
@@ -9,7 +9,7 @@ class OfficialLetterheadTest extends TestCase
 {
     public function test_official_asset_has_expected_dimensions_and_checksum(): void
     {
-        $path = dirname(__DIR__, 2).'/resources/images/documents/papel-timbrado.png';
+        $path = dirname(__DIR__, 2).'/resources/images/documents/papel-timbrado-novo.png';
         $size = getimagesize($path);
 
         $this->assertSame([OfficialLetterhead::WIDTH, OfficialLetterhead::HEIGHT], [$size[0], $size[1]]);
