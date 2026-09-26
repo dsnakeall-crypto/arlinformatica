4a548b9e090edd37657063b636830d43e1cb4bda
dsnakeall-crypto <dsnakeall@gmail.com>
2026-09-26 02:21:08 -0300

    fix: voltar papel timbrado para jpg

--- app/Services/OfficialLetterhead.php
+++ app/Services/OfficialLetterhead.php
@@ -10,13 +10,13 @@ class OfficialLetterhead
 
     public const HEIGHT = 1491;
 
-    public const SHA256 = '68a8da62efcebb7713c0346d5c9946977157e2583263a822ecfb461bac4eda2a';
+    public const SHA256 = '47e3b260bd34547d486ddf54af777e085ff7ed73448f8b5985e5e4c1476b8336';
 
     public static function path(): string
     {
         // This service is also used by a plain PHPUnit test, before Laravel's
         // application container (and therefore resource_path()) is available.
-        $path = dirname(__DIR__, 2).'/resources/images/documents/papel-timbrado.png';
+        $path = dirname(__DIR__, 2).'/resources/images/documents/papel-timbrado.jpg';
         $size = @getimagesize($path);
 
         if ($size === false || $size[0] !== self::WIDTH || $size[1] !== self::HEIGHT || hash_file('sha256', $path) !== self::SHA256) {
